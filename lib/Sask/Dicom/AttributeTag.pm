354c31b6aeab5b710777ae4cc854e240530740c9
Ashley Pond V <ashley.pond.v@gmail.com>
2011-06-15 11:18:07 -0700

    Pulled dupe and added comments re construction args.

--- lib/Sask/Dicom/AttributeTag.pm
+++ lib/Sask/Dicom/AttributeTag.pm
@@ -10,10 +10,6 @@ subtype "GroupInt"
     => as "Int"
     => where { $_ >= 2 and $_ <= 0xffff };
 
-subtype "GroupInt"
-    => as "Int"
-    => where { $_ >= 2 and $_ <= 0xffff };
-
 subtype "ElementInt"
     => as "Int"
     => where { $_ >= 0 and $_ <= 0xffff };
@@ -22,9 +18,9 @@ use overload q{""} => sub { +shift->tag }, fallback => 1;
 
 sub BUILDARGS {
     my $class = shift;
-    my %arg = @_ == 1 ? %{ $_[0] } :
-        @_ == 2 ? ( group => $_[0], element => $_[1] ) :
-        @_ == 4 ? @_ :
+    my %arg = @_ == 1 ? %{ $_[0] } :                     # hashref
+        @_ == 2 ? ( group => $_[0], element => $_[1] ) : # ordered list
+        @_ == 4 ? @_ :                                   # hash
         confess "Bad arguments to new: ", Data::Dump::dump(@_);
     $_ = hex($_) for values %arg;
     \%arg;
