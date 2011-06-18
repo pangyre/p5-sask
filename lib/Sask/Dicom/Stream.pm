f2d0c3a56d769208daa0a0b1ed0998f94c306153
Ashley Pond V <apv@cassidy.local>
2011-06-18 11:29:50 -0700

    Reverting to direct file operations. Skip object versions.

--- lib/Sask/Dicom/Stream.pm
+++ lib/Sask/Dicom/Stream.pm
@@ -2,6 +2,7 @@ package Sask::Dicom::Stream;
 use Mouse;
 use autodie;
 use IO::Handle;
+use IO::File;
 use IO::String;
 use Carp;
 
@@ -12,8 +13,7 @@ has "io" =>
     is => "ro",
     isa => "FileHandle", # |IO::String|IO::Handle",
     required => 1,
-#?    lazy => 1,
-    handles => [qw( sysseek sysread )],
+#    handles => [qw( sysseek sysread )],
     trigger => sub { binmode +shift->{io}, ":bytes" },
     ;
 
@@ -56,12 +56,13 @@ sub explicit_vr {
 
 sub read_meta_information {
     my $self = shift;
-    sysseek($self->io,128,0);
     my $buf;
-    sysread($self->io, $buf, 4) == 4 or die "Couldn't seek to end of header";
+    my $io = $self->io;
+    sysseek($io,128,0);
+    sysread($io,$buf, 4) == 4 or die "Couldn't seek to end of header";
     $buf eq "DICM" or croak "Not a recognizable DICOM file...";
 
-    $self->io->sysread($buf,2) == 2 or die;
+    sysread($io,$buf,2) == 2 or die;
 
     my $endian = unpack("v",$buf) == 2 ?
         "little" : unpack("n",$buf) == 2 ?
@@ -71,10 +72,10 @@ sub read_meta_information {
     my $us_template = $endian eq "little" ? "v" : "n";
     my $ul_template = $endian eq "little" ? "V" : "N";
 
-    sysseek($self->io,-2,1); # Rewind for symmetric parsing.
+    sysseek($io,-2,1); # Rewind for symmetric parsing.
 
     my $i;
-    while ( $self->io->sysread($buf,6) )
+    while ( sysread($io,$buf,6) )
     {
         my ( $group, $element, $vr ) = unpack "${us_template}2 A2", $buf;
         $vr = eval { vr($vr) };
@@ -84,13 +85,13 @@ sub read_meta_information {
         my $length;
         if ( $vr->fml )
         {
-            $self->io->sysread($buf,2); # Reserved, toss. seek instead?
-            $self->io->sysread($buf,4);
+            sysseek($io,2,1);
+            sysread($io, $buf,4);
             $length = unpack $ul_template, $buf;
         }
         else
         {
-            $self->io->sysread($buf,2);
+            sysread($io,$buf,2);
             $length = unpack $us_template, $buf;
         }
         printf("length -> %s\n", $length);
